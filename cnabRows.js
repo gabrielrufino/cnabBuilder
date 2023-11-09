d08460183b99e7b18780c27a671c38b125ae3722
Gabriel Rufino <contato@gabrielrufino.com>
2023-11-09 11:30:46 -0300

    Support for custom file

--- cnabRows.js
+++ cnabRows.js
@@ -13,14 +13,19 @@ const optionsYargs = yargs(process.argv.slice(2))
   .option("f", { alias: "from", describe: "posição inicial de pesquisa da linha do Cnab", type: "number", demandOption: true })
   .option("t", { alias: "to", describe: "posição final de pesquisa da linha do Cnab", type: "number", demandOption: true })
   .option("s", { alias: "segmento", describe: "tipo de segmento", type: "string", demandOption: true })
+  .option('file', { describe: "caminho para o arquivo", type: "string" })
   .example('$0 -f 21 -t 34 -s p', 'lista a linha e campo que from e to do cnab')
   .argv;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const file = path.resolve(`${__dirname}/cnabExample.rem`)
 
-const { from, to, segmento } = optionsYargs
+const {
+  from,
+  to,
+  segmento,
+  file = path.resolve(`${__dirname}/cnabExample.rem`)
+} = optionsYargs
 
 const sliceArrayPosition = (arr, ...positions) => [...arr].slice(...positions)
 
